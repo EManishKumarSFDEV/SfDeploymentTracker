9d9727ef7ff39c835e4e6bec150e2531de156984
E Manish <emkrock1@gmail.com>
2024-09-21 20:14:08 +0530

    page tx update

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,4 +1,4 @@
-import { DeploymentTracker } from "@/components/deployment-tracker"
+import DeploymentTracker from "@/components/deployment-tracker"
 
 export default function Page() {
   return <DeploymentTracker />
